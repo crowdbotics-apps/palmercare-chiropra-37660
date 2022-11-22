8b1b22e748bfc17ab9a8cc2e73d13d0e8dbdb230
Crowdbotics <team@crowdbotics.com>
2022-11-22 13:31:59 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/palmercare_chiropra_37660/MainActivity.java
@@ -0,0 +1,15 @@
+package com.palmercare_chiropra_37660;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "palmercare_chiropra_37660";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "palmercare_chiropra_37660",
+  url: "https://palmercare_chiropra_37660.botics.co",
+  api: "https://palmercare_chiropra_37660.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
